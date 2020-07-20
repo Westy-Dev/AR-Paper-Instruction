636b57106358242f133acee9ee2c1c9b0b42cdb6
BenWestcott <ben.westcott@gmail.com>
2020-07-20 15:49:16 +0100

    Added all instruction pages for Sports Car kit 31100. Add UI to move between pages and implemented moving between pages functionality

---
+++ Assets/Scripts/InstructionsManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionsManager : MonoBehaviour
+{
+    public Texture[] InstructionPages;
+    public GameObject InstructionPanel;
+    private int currentPageIndex;
+    private int lastPageIndex;
+    private Renderer instructionPanelRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPageIndex = InstructionPages.Length;
+        currentPageIndex = 0;
+        instructionPanelRenderer = InstructionPanel.GetComponent<Renderer>();
+        instructionPanelRenderer.material.mainTexture = InstructionPages[currentPageIndex];
+    }
+
+    public void loadNextInstruction()
+    {
+        if (currentPageIndex != lastPageIndex)
+        {
+            currentPageIndex++;
+            instructionPanelRenderer.material.mainTexture = InstructionPages[currentPageIndex];
+        }
+   
+    }
+
+    public void loadPreviousInstruction()
+    {
+        if(currentPageIndex != 0)
+        {
+            currentPageIndex--;
+            instructionPanelRenderer.material.mainTexture = InstructionPages[currentPageIndex];
+        }  
+    }
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public InstructionsManager instructionsManager;
+
+    public void loadNextInstruction()
+    {
+        instructionsManager.loadNextInstruction();
+    }
+
+    public void loadPreviousInstruction()
+    {
+        instructionsManager.loadPreviousInstruction();
+    }
+}
+
