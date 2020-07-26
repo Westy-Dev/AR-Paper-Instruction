6fc2953468d8a2526c32aa28e1cd2a2ca457fda4
BenWestcott <ben.westcott@gmail.com>
2020-07-26 14:14:51 +0100

    Added scale and move functions to gesture manager

--- Assets/TouchGestureManager.cs
+++ Assets/TouchGestureManager.cs
@@ -6,37 +6,68 @@ public class TouchGestureManager : MonoBehaviour
     [SerializeField]
     private Camera arCamera;
 
-    private Touch touch;
-    private Vector2 touchPosition;
     private GameObject arObject;
+    private float movementSensitivity = 0.001f;
+
+    float initialFingersDistance;
+    Vector3 initialArObjectScale;
     // Start is called before the first frame update
     void Start()
     {
-      
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.touchCount > 0)
+        if (Input.touchCount == 2)
         {
-            Debug.Log("Touch Phase = " + touch.phase);
+            Touch touch1 = Input.touches[0];
+            Touch touch2 = Input.touches[1];
 
-            touch = Input.GetTouch(0);
-            touchPosition = touch.position;
+            if (touch1.phase == TouchPhase.Began || touch2.phase == TouchPhase.Began)
+            {
+                initialFingersDistance = Vector2.Distance(touch1.position, touch2.position);
+                Ray ray = arCamera.ScreenPointToRay(touch1.position);
+                RaycastHit hitARObject;
+                //Debug.Log("Ray Fired");
+                if (Physics.Raycast(ray, out hitARObject))
+                {
+                    arObject = hitARObject.transform.gameObject;
+                    initialArObjectScale = arObject.transform.localScale;
+                }
+            }
+            else if (touch1.phase == TouchPhase.Moved || touch2.phase == TouchPhase.Moved)
+            {
+                var currentFingersDistance = Vector2.Distance(touch1.position, touch2.position);
+                var scaleFactor = currentFingersDistance / initialFingersDistance;
+                if (arObject != null)
+                {
+                    arObject.transform.localScale = initialArObjectScale * scaleFactor;
+                }
 
-            if(touch.phase == TouchPhase.Began)
+            }
+
+        }
+        else if (Input.touchCount == 1)
+        {
+            //Debug.Log("Touch Phase = " + touch.phase);
+
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = touch.position;
+
+            if (touch.phase == TouchPhase.Began)
             {
                 Ray ray = arCamera.ScreenPointToRay(touchPosition);
                 RaycastHit hitARObject;
-                Debug.Log("Ray Fired");
-                if(Physics.Raycast(ray, out hitARObject))
+                //Debug.Log("Ray Fired");
+                if (Physics.Raycast(ray, out hitARObject))
                 {
                     arObject = hitARObject.transform.gameObject;
-                    if(arObject != null)
+                    if (arObject != null)
                     {
-                        Debug.Log("Hit Object");
-                        Debug.Log("Position = " + arObject.transform.position);
+                        // Debug.Log("Hit Object");
+                        //  Debug.Log("Position = " + arObject.transform.position);
                     }
                 }
             }
@@ -45,12 +76,12 @@ public class TouchGestureManager : MonoBehaviour
             {
                 if (arObject != null)
                 {
-                    Vector2 touchMovement = arCamera.ScreenToWorldPoint(touch.deltaPosition)/10;
+                    Vector2 touchMovement = (touch.deltaPosition) * movementSensitivity;
                     Debug.Log("Delta Touch Position = " + touchMovement);
                     arObject.transform.position = new Vector3(
                                 arObject.transform.position.x,
                                 arObject.transform.position.y + touchMovement.y,
-                                arObject.transform.position.z + touchMovement.x);
+                               arObject.transform.position.z + touchMovement.x);
                     Debug.Log("New Position = " + arObject.transform.position);
                 }
             }
